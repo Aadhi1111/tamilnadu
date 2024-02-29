499b898239170bb915d97214bd55f290dceb49d8
Aadhi1111 <94366686+Aadhi1111@users.noreply.github.com>
2024-03-01 03:38:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function f1(){
+    console.log("vera maari");
+    function f2(){
+        console.log("new maari");
+        function f3(){
+            console.log("maari");
+            function f4(){
+                console.log("Hello World");
+            }
+        }
+    }
+}
+
+f1();
